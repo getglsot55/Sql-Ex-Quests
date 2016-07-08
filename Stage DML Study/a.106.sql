55548456b48419c544e787c88501cedf6dffa35c
Montigomo <agitech@gmail.com>
2016-07-08 14:41:44 +0300

    2.8

--- Stage DML Study/a.106.sql
+++ Stage DML Study/a.106.sql
@@ -1,7 +1,42 @@
 
 
--- ѕусть v1, v2, v3, v4, ... представл€ет последовательность вещественных чисел - объемов окрасок b_vol, упор€доченных по возрастанию b_datetime, b_q_id, b_v_id. 
+-- ѕусть v1, v2, v3, v4, ... представл€ет последовательность вещественных чисел - объемов окрасок b_vol, 
+-- упор€доченных по возрастанию b_datetime, b_q_id, b_v_id. 
 -- Ќайти преобразованную последовательность P1=v1, P2=v1/v2, P3=v1/v2*v3, P4=v1/v2*v3/v4, ..., 
 -- где каждый следующий член получаетс€ из предыдущего умножением на vi (при нечетных i) или делением на vi (при четных i). 
 -- –езультаты представить в виде b_datetime, b_q_id, b_v_id, b_vol, Pi, где Pi - член последовательности, 
 -- соответствующий номеру записи i. ¬ывести Pi с 8-ю знаками после зап€той. 
+
+-- cost 0.049351383000612 operations 25 SQS - 20 ms
+with cte1 as (
+select	row_number() over(order by b_datetime, b_q_id, b_v_id) rn,
+	(select count(*) from dbo.utB) as rc,	b_datetime, b_q_id, b_v_id, b_vol
+from dbo.utB),
+cte2 as (
+select [rn], b_datetime, b_q_id, b_v_id, b_vol, cast(b_vol as float) as [Pi]
+from cte1 where [rn] = 1
+union all
+select cte2.[rn] + 1, cte1.b_datetime, cte1.b_q_id, cte1.b_v_id, cte1.b_vol,
+case when (cte2.[rn] + 1) % 2 = 0 then cte2.[Pi] / cast(cte1.[b_vol] as float) else cte2.[Pi] * cast(cte1.[b_vol] as float) end as [Pi]
+from cte2 join cte1 on cte1.rn  = (cte2.rn + 1)
+where cte2.rn <= rc)
+select b_datetime, b_q_id, b_v_id, b_vol, cast([Pi] as decimal(18,8)) 
+from cte2 OPTION (MAXRECURSION 300);
+
+
+-- cost 0.049351383000612 operations 25 SQS - 20 ms
+with cte1 as (
+select	row_number() over(order by b_datetime, b_q_id, b_v_id) rn,
+	count(*) over() as rc,	b_datetime, b_q_id, b_v_id, b_vol
+from dbo.utB),
+cte2 as (
+select [rn], b_datetime, b_q_id, b_v_id, b_vol, cast(b_vol as float) as [Pi]
+from cte1 where [rn] = 1
+union all
+select cte2.[rn] + 1, cte1.b_datetime, cte1.b_q_id, cte1.b_v_id, cte1.b_vol,
+case
+	when (cte2.[rn] + 1) % 2 = 0 then cte2.[Pi] / cast(cte1.[b_vol] as float) else cte2.[Pi] * cast(cte1.[b_vol] as float)
+end as [Pi]
+from cte2 join cte1 on cte1.rn  = (cte2.rn + 1)
+where cte2.rn <= rc)
+select b_datetime, b_q_id, b_v_id, b_vol, cast([Pi] as decimal(18,8)) from cte2 OPTION (MAXRECURSION 300);
---
+++ Stage DML Study/a.109.sql
@@ -0,0 +1,62 @@
+﻿
+
+-- Вывести:
+-- 1. Названия всех квадратов черного или белого цвета.
+-- 2. Общее количество белых квадратов.
+-- 3. Общее количество черных квадратов
+
+--select *
+--from dbo.utQ left join dbo.utB on utB.B_Q_ID = utQ.Q_ID left join dbo.utV on utV.V_ID = utB.B_V_ID
+--group by B_Q_ID, V_COLOR
+
+
+--select Q_ID, sum(B_VOL), V_COLOR
+--from dbo.utQ left join dbo.utB on utB.B_Q_ID = utQ.Q_ID left join dbo.utV on utV.V_ID = utB.B_V_ID
+--group by Q_ID, V_COLOR
+
+-- cost 0.072603791952133 operations 17 
+
+--DBCC DROPCLEANBUFFERS
+--DBCC FREEPROCCACHE
+--GO
+--SET STATISTICS IO ON;
+--SET STATISTICS TIME ON;
+--GO
+
+select tr.Q_NAME,
+	sum(case when Col = 'W' then 1 else 0 end) over() [Whites],
+	sum(case when Col = 'B' then 1 else 0 end) over() [Blacks]
+from (
+select pvt.Q_NAME,
+	case
+		when coalesce(pvt.R, 0) = 255 and coalesce(pvt.G, 0) = 255 and coalesce(pvt.B, 0) = 255 then 'W'
+		when coalesce(pvt.R, 0) = 0 and coalesce(pvt.G, 0) = 0 and coalesce(pvt.B, 0) = 0 then 'B'
+		else 'S'
+	end as Col
+from (
+select Q_ID, Q_NAME, B_VOL, V_COLOR
+from dbo.utQ left join dbo.utB on utB.B_Q_ID = utQ.Q_ID left join dbo.utV on utV.V_ID = utB.B_V_ID) t1
+pivot(sum(B_VOL) for V_COLOR in ([R], [G], [B])) pvt ) tr
+where Col = 'W' or Col = 'B'
+
+--SET STATISTICS IO OFF;
+--SET STATISTICS TIME OFF;
+--GO
+
+
+with tr as (
+select pvt.Q_NAME,
+	case
+		when coalesce(pvt.R, 0) = 255 and coalesce(pvt.G, 0) = 255 and coalesce(pvt.B, 0) = 255 then 'W'
+		when coalesce(pvt.R, 0) = 0 and coalesce(pvt.G, 0) = 0 and coalesce(pvt.B, 0) = 0 then 'B'
+		else 'S'
+	end as Col
+from (
+select Q_ID, Q_NAME, B_VOL, V_COLOR
+from dbo.utQ left join dbo.utB on utB.B_Q_ID = utQ.Q_ID left join dbo.utV on utV.V_ID = utB.B_V_ID) t1
+pivot(sum(B_VOL) for V_COLOR in ([R], [G], [B])) pvt ) 
+select tr.Q_NAME,
+	sum(case when Col = 'W' then 1 else 0 end) over() [Whites],
+	sum(case when Col = 'B' then 1 else 0 end) over() [Blacks]
+from tr
+where Col = 'W' or Col = 'B'
