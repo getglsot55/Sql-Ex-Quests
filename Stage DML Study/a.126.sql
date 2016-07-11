f0c67c497e11aee87df9d99182cc1823ef7d3207
Montigomo <agitech@gmail.com>
2016-07-11 16:42:51 +0300

    2.8

---
+++ Stage DML Study/a.126.sql
@@ -0,0 +1,24 @@
+﻿
+
+
+-- Для последовательности пассажиров, упорядоченных по id_psg, определить того, 
+-- кто совершил наибольшее число полетов, а также тех, кто находится в последовательности непосредственно перед и после него.
+-- Для первого пассажира в последовательности предыдущим будет последний, а для последнего пассажира последующим будет первый.
+-- Для каждого пассажира, отвечающего условию, вывести: имя, имя предыдущего пассажира, имя следующего пассажира. 
+
+-- cost 0.048182006925344 operations 59 
+with cte as (select [ID_psg], count(*) as ct, max(count(*)) over() as mct
+from [dbo].[Pass_in_trip]
+group by [ID_psg]),
+ctf as (select cte.*, [dbo].[Passenger].[name] from cte join [dbo].[Passenger] on [Passenger].[ID_psg] = [cte].[ID_psg] where ct = mct),
+ctg as (select row_number() over(order by [ID_psg]) as rn, count(*) over() as cr,  [ID_psg], [name] from [dbo].[Passenger])
+select ctf.[name] as [psg],
+	case when ctg.[rn] = 1 
+		then (select name from ctg ga where ga.[rn] = ctg.[cr]) 
+		else (select name from ctg ga where ga.[rn] = ctg.[rn] - 1)
+	end as [prev],
+	case when ctg.[rn] = ctg.[cr]
+		then (select name from ctg ga where ga.[rn] = 1)
+		else (select name from ctg ga where ga.[rn] = ctg.[rn] + 1)
+	end as [next]
+from ctf join ctg on [ctg].[ID_psg] = [ctf].[ID_psg];
---
+++ Stage DML Study/a.127.sql
@@ -0,0 +1,35 @@
+﻿
+
+-- Найти округленное до сотых долей среднее арифметическое следующих цен:
+-- 1. Цена самых дешевых Laptop-ов от производителей РС с самой низкой скоростью CD;
+-- 2. Цена самых дорогих РС от производителей самых дешевых принтеров;
+-- 3. Цена самых дорогих принтеров от производителей Laptop-ов с наибольшим объемом памяти.
+-- Замечание: При расчёте среднего отсутствующие цены не учитывать. 
+
+
+-- cost 0.092657446861267 operations 44 
+with cte as (select
+(select top 1 price
+from [dbo].[Laptop] join [dbo].[Product] on [Product].[model] = [Laptop].[model]
+where [maker] in (
+select maker
+from pc join [dbo].[Product] on [Product].[model] = [PC].[model]
+where cd = (select min(cd) from pc)) order by price) [1],
+(select top 1 price
+from [dbo].[PC] join [dbo].[Product] on [Product].[model] = [PC].[model]
+where [maker] in (
+select maker
+from [dbo].[Printer] join [dbo].[Product] on [Product].[model] = [Printer].[model]
+where [price] = (select min([price]) from [dbo].[Printer])) order by price desc) [2],
+(select top 1 price
+from [dbo].[Printer] join [dbo].[Product] on [Product].[model] = [Printer].[model]
+where [maker] in (
+select maker
+from [dbo].[Laptop] join [dbo].[Product] on [Product].[model] = [Laptop].[model]
+where [ram] = (select max([ram]) from [dbo].[Laptop])) order by price desc) [3])
+select cast(val / iif([ca].[div] = 0, 1, [ca].[div]) as decimal(14, 2))
+from cte
+cross apply (
+select (coalesce([cte].[1], 0) * 1.0 + coalesce([cte].[2], 0) * 1.0 + coalesce([cte].[3], 0) * 1.0) as val,
+iif([cte].[1] is null, 0, 1) + iif([cte].[2] is null, 0, 1) + iif([cte].[3] is null, 0, 1) as [div]
+from cte) ca;
